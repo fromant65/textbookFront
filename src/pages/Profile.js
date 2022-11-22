7537432a53ad252aa629d3320bcbe55d7c0497e0
fromant <fromant65@gmail.com>
2022-11-22 16:30:13 -0300

    Agregados comentarios en Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,9 +4,11 @@ import "../css/Navbar.css";
 import { getUsername } from "../getUsername";
 
 const Profile = ({ username }) => {
+  //Username es una prop opcional que hace referencia a quién es el dueño del perfil
   const [user, setUser] = useState("");
   useEffect(() => {
     const setProfileUser = async () => {
+      //En caso de que prop no esté definida, quiere decir que accedimos a nuestro propio perfil
       const profileUser = username ? username : await getUsername();
       setUser(profileUser);
     };
