a9b1cea91c0cc9b6aa5efe8e973a526e258d5dff
fromant <fromant65@gmail.com>
2022-11-26 16:04:18 -0300

    Mejora hecha al 404

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { serverLink } from "../App";
 import "../css/NotFound.css";
 
 const NotFound = () => {
+  const [isLogged, setIsLogged] = useState(false);
+  useEffect(() => {
+    fetch(`${serverLink}/login`, { credentials: "include" })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.loggedIn) setIsLogged(true);
+      });
+  }, [isLogged]);
   return (
     <section className="not-found-container">
       <h1 className="not-found-title">
         Parece que la página que buscabas no está disponible...
       </h1>
-      <a className="not-found-link" href="/">
+      <a className="not-found-link" href={isLogged ? "/home" : "/"}>
         Volver a la página de inicio
       </a>
     </section>
